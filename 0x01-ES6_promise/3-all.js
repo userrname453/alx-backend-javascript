6d4c72f3bdb6c1914afe491465078d4b9b0efd1b
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-03 03:06:22 +0100

    dfg

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,10 @@
-/* eslint-disable no-console */
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => console.error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
