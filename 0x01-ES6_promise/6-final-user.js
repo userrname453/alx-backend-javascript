889af752b23238b9eaf445364aee23fcc81b8cc6
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-03 03:25:31 +0100

    sadf

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  // create an array of promises to pass to the Promise.allSettled()
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  // use .allSettled to wait for promises results(success or failure)
+  // and then we return the array describing the outcome of each promise
+  return Promise.allSettled(promises).then((results) =>
+    // map over results
+    // eslint-disable-next-line implicit-arrow-linebreak
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
+}
