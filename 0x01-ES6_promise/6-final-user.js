f0a705cc95237aa49587008624d8c2a32099e96c
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-03 03:26:44 +0100

    fgh

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   // create an array of promises to pass to the Promise.allSettled()
   const promises = [
     signUpUser(firstName, lastName),
