0ed05c945168949a4b60c9db48705a2dfc31f88c
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-05 02:35:50 +0100

    sf

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -18,7 +18,7 @@ class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
 
