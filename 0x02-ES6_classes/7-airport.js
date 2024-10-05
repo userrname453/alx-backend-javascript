ee6846c8e1ab4ad8d1bd9f1f17cb952cbb95c361
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-05 02:55:10 +0100

    xcv

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,35 @@
+class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+    console.log(this._name);
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}
+
+export default Airport;
