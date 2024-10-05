4867e39321ae9afd1636652db8712d733c887dbf
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-05 03:41:07 +0100

    sfasdf

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    console.log(Symbol);
+  }
+
+  cloneCar() {
+    const Constructor = Symbol.for(this.constructor.name);
+    return new (this[Constructor] || this.constructor)();
+  }
+
+  [Symbol.for('Car')]() {
+    return this.constructor;
+  }
+}
