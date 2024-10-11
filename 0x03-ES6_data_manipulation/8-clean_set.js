3e41e5ddfb59964ab9d94334307d429059a59b4f
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-11 09:53:24 +0100

    sdf

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 const cleanSet = (set, startString) => {
+  if (!startString) return '';
   const arr = Array.from(set);
   const filtered = arr.filter((e) => e.startsWith(startString));
   const mapped = filtered.map((e) => e.slice(startString.length));
