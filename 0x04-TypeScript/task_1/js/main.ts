427abaa0e41ad5bbdc34ad10a8a65b31fa7c49cb
Mohamed Sboui <akramsboui453@gmail.com>
2024-10-12 00:01:23 +0100

    finish task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,30 +4,72 @@ interface Teacher {
     fullTimeEmployee: boolean;
     location: string;
     yearsOfExperience?: number;
-    [key: string]: any; 
-  }
-  
-  interface Directors extends Teacher {
+    [key: string]: any;
+}
+
+interface Directors extends Teacher {
     numberOfReports: number;
-  }
-  
-  const teacher3: Teacher = {
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+const teacher3: Teacher = {
     firstName: 'John',
     lastName: 'Doe',
     fullTimeEmployee: false,
     location: 'London',
     contract: false,
-  };
-  
-  console.log(teacher3);
-  
-  const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
+};
+
+console.log(teacher3);
+
+const director1: Directors = {
+    firstName: 'Jane',
+    lastName: 'Smith',
     location: 'London',
     fullTimeEmployee: true,
     numberOfReports: 17,
-  };
-  
-  console.log(director1);
-  
+};
+
+console.log(director1);
+
+console.log(printTeacher("John", "Doe"));
+
+interface Student {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentClassInterface {
+    new(firstName: string, lastName: string): Student;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+const student1 = new StudentClass("John", "Doe");
+console.log(student1.displayName());
+console.log(student1.workOnHomework());
